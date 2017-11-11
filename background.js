accba92399ce3217e53b81190272a4e1e7828e66
nazaninreihani <nazanin.reyh@gmail.com>
2017-11-11 18:58:25 +0330

    add babel and convert es5 to es6

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 // When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(() => {
   // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     // With a new rule ...
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        // That fires when a page's URL contains a 'g' ...
+        // That fires when a page's URL contains a '.binary.com' ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
             pageUrl: { urlContains: '.binary.com' },
@@ -18,18 +18,17 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.sync.get(['app_id'], function(res) {
-        document.getElementById('app-id').value = res.app_id;
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.sync.get(['app_id'], res => {
+        document.getElementById('app-id').value = res.app_id || 1098;
     });
-  chrome.storage.sync.get(['server-url'], function(res) {
+  chrome.storage.sync.get(['server-url'], res => {
     document.getElementById('server-url').value = res.server_url || 'blue.binaryws.com';
   });
 });
 
 
-chrome.storage.onChanged.addListener(function(changes, area) {
-  console.log(changes);
+chrome.storage.onChanged.addListener((changes, area) => {
   if (area == "sync" && "app_id" in changes) {
     console.log(changes.app_id.newValue);
     appID = changes.app_id.newValue;
@@ -38,7 +37,6 @@ chrome.storage.onChanged.addListener(function(changes, area) {
     document.getElementById('server-url').value = changes.server_url.newValue;
   }
 });
-
 
 
 
